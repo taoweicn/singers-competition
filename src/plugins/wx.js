3005114ecd6ed5b24c586280423c15a8e636ce6e
taoweicn <twchn@live.com>
2018-05-27 02:56:05 +0800

    🐛 fix: add share URL encode

--- src/plugins/wx.js
+++ src/plugins/wx.js
@@ -21,9 +21,13 @@ getWXconfig().then((res) => {
 const wxShareConfig = {
   title: '测测哪位十佳歌手最合你的音乐品味', // 分享标题
   desc: '我最匹配的十大歌手', // 分享描述
-  link: 'https://weixin.bingyan-tech.hustonline.net/top-singers-index/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+  link: encodeURIComponent(window.location.href.split('#')[0]), // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
   imgUrl: `https://weixin.bingyan-tech.hustonline.net/top-singers-index/${shareIcon}` // 分享图标
 };
+
+wx.error(() => {
+  console.log(1);
+});
 
 const wxShare = (config = wxShareConfig) => {
   wx.onMenuShareTimeline(config);
