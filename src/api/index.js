845b425d98f59fcb4e1352c30621af91d318d697
taoweicn <twchn@live.com>
2018-05-27 02:32:06 +0800

    🐛 fix: modify api address

--- src/api/index.js
+++ src/api/index.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 import { getLocal } from '@/utils/cache';
 
 const userInfo = axios.create({
-  baseURL: '/top-singers-index/api/v1',
+  baseURL: '/api/v1',
   headers: {
     Authorization: getLocal('token')
   }
 });
 
-export const judgeStatus = () => userInfo.get('user/status');
+export const judgeStatus = () => userInfo.get('/user/status');
 
-export const getWXconfig = () => axios.get('service/resources/signature', {
+export const getWXconfig = () => axios.get('/service/resources/signature', {
   params: {
     url: window.location.href.split('#')[0]
   }
