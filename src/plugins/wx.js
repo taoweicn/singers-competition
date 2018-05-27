15b23927ab1ba834b1cfcae108d708726faa71d5
taoweicn <twchn@live.com>
2018-05-27 20:25:54 +0800

    💄 fix: change share desc

--- src/plugins/wx.js
+++ src/plugins/wx.js
@@ -24,7 +24,7 @@ export default async () => {
       console.log('wx ready');
       const wxShareConfig = {
         title: '最合我音乐品味的十佳歌手原来是ta！快来一起测测吧！', // 分享标题
-        desc: '通过独一无二的感知测试，生成你的音乐品味说明书。', // 分享描述
+        desc: '通过独一无二的感知测试，生成你的音乐品味鉴定书。', // 分享描述
         link: 'https://weixin.bingyan-tech.hustonline.net/top-singers-index/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
         imgUrl: `https://weixin.bingyan-tech.hustonline.net${shareIcon}` // 分享图标
       };
