4a753cb29d3267537f70aa1cf1a7fc289e824b7f
taoweicn <twchn@live.com>
2018-05-27 18:34:53 +0800

    🐛 fix: add log

--- src/pages/ResultPage/index.vue
+++ src/pages/ResultPage/index.vue
@@ -197,17 +197,12 @@ export default {
       }, 1000);
     },
     renderSharePicture(username, resultText, avatarURL, radarMapDataURL, singersURL) {
-      alert(username);
-      alert(avatarURL);
       if (typeof username !== 'string') return;
       const background = new Image();
       background.src = sharePicture;
       const canvas = document.createElement('canvas');
       const content = canvas.getContext('2d');
-      // 判断缓存
-      alert(background.complete);
-      if (background.complete) {
-        console.log(background);
+      background.onload = () => {
         canvas.width = background.width;
         canvas.height = background.height;
         content.drawImage(background, 0, 0); // 绘制背景图片
@@ -217,34 +212,14 @@ export default {
         content.fillStyle = '#4f3e2f';
         content.font = '28px/1.4 PingFang-SC-Bold';
         this.wrapText(content, resultText, 88, 480, 285, 39); // 绘制结果文字
-        this.drawImage(content, avatarURL, 60, 344, 83, 83); // 绘制用户头像
+        // this.drawImage(content, avatarURL, 60, 344, 83, 83); // 绘制用户头像
         this.drawImage(content, radarMapDataURL, 380, 500, 330, 250); // 绘制雷达图
         this.drawImage(content, singersURL, 132, 899, 371, 205); // 绘制歌手图片
         setTimeout(() => {
           this.sharePictureURL = canvas.toDataURL();
           alert(this.sharePictureURL);
         }, 1000);
-      } else {
-        background.onload = () => {
-          alert('loaded');
-          canvas.width = background.width;
-          canvas.height = background.height;
-          content.drawImage(background, 0, 0); // 绘制背景图片
-          content.fillStyle = '#fff';
-          content.font = '32px PingFang-SC-Bold';
-          content.fillText(username, 165, 375); // 绘制用户名
-          content.fillStyle = '#4f3e2f';
-          content.font = '28px/1.4 PingFang-SC-Bold';
-          this.wrapText(content, resultText, 88, 480, 285, 39); // 绘制结果文字
-          this.drawImage(content, avatarURL, 60, 344, 83, 83); // 绘制用户头像
-          this.drawImage(content, radarMapDataURL, 380, 500, 330, 250); // 绘制雷达图
-          this.drawImage(content, singersURL, 132, 899, 371, 205); // 绘制歌手图片
-          setTimeout(() => {
-            this.sharePictureURL = canvas.toDataURL();
-            alert(this.sharePictureURL);
-          }, 1000);
-        };
-      }
+      };
     },
     drawImage(content, url, x, y, width, height) {
       const image = new Image(width, height);
@@ -298,11 +273,7 @@ export default {
         if (res.data.status) {
           this.username = res.data.data.nickname;
           this.avatar = res.data.data.headimgurl;
-          alert(this.username);
-          alert(this.avatar);
-          this.$nextTick(() => {
-            this.renderRadarMap();
-          });
+          this.renderRadarMap();
           setTimeout(() => {
             this.isLoading = false;
           }, 3000);
