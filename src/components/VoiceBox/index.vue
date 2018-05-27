9562ccf34dc6797d3fcb69e25242f8055b3418f4
taoweicn <twchn@live.com>
2018-05-27 19:57:38 +0800

    🐛 fix: show audio duration

--- src/components/VoiceBox/index.vue
+++ src/components/VoiceBox/index.vue
@@ -56,7 +56,13 @@ export default {
   mounted() {
     const audio = new Audio();
     audio.src = this.audioURL;
-    audio.onloadeddata = () => {
+    audio.ondurationchange = () => {
+      this.time = this.$refs.audio.duration;
+    };
+    audio.onloadedmetadata = () => {
+      this.time = this.$refs.audio.duration;
+    };
+    audio.oncanplay = () => {
       this.time = this.$refs.audio.duration;
     };
   }
