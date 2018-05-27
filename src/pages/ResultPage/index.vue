77225908c8fd7b351b1e13b117800992556c3c9e
taoweicn <twchn@live.com>
2018-05-27 18:01:39 +0800

    🐛 fix: image not onload

--- src/pages/ResultPage/index.vue
+++ src/pages/ResultPage/index.vue
@@ -198,11 +198,29 @@ export default {
     },
     renderSharePicture(username, resultText, avatarURL, radarMapDataURL, singersURL) {
       alert(username);
-      // if (typeof username !== 'string') return;
+      if (typeof username !== 'string') return;
       const background = new Image();
       background.src = sharePicture;
       const canvas = document.createElement('canvas');
       const content = canvas.getContext('2d');
+      if (this.isWeiXin) {
+        canvas.width = background.width;
+        canvas.height = background.height;
+        content.drawImage(background, 0, 0); // 绘制背景图片
+        content.fillStyle = '#fff';
+        content.font = '32px PingFang-SC-Bold';
+        content.fillText(username, 165, 375); // 绘制用户名
+        content.fillStyle = '#4f3e2f';
+        content.font = '28px/1.4 PingFang-SC-Bold';
+        this.wrapText(content, resultText, 88, 480, 285, 39); // 绘制结果文字
+        this.drawImage(content, avatarURL, 60, 344, 83, 83); // 绘制用户头像
+        this.drawImage(content, radarMapDataURL, 380, 500, 330, 250); // 绘制雷达图
+        this.drawImage(content, singersURL, 132, 899, 371, 205); // 绘制歌手图片
+        setTimeout(() => {
+          this.sharePictureURL = canvas.toDataURL();
+        }, 1000);
+        return;
+      }
       background.onload = () => {
         canvas.width = background.width;
         canvas.height = background.height;
