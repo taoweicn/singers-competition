01dc94b47adb6724e3e7ce6dc70fa28636acaa9b
taoweicn <twchn@live.com>
2019-07-20 13:26:14 +0800

    🐛 fix: fixed cache utils

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,29 +1,29 @@
 export function setLocal(name, content) {
-  if (!name && typeof name !== 'string' && content != null) return;
+  if (typeof name !== 'string' || !name || content == null) return;
   localStorage.setItem(name, JSON.stringify(content));
 }
 
 export function getLocal(name) {
-  if (!name && typeof name !== 'string') return null;
+  if (typeof name !== 'string' || !name) return null;
   return JSON.parse(localStorage.getItem(name));
 }
 
 export function removeLocal(name) {
-  if (!name && typeof name !== 'string') return;
+  if (typeof name !== 'string' || !name) return;
   localStorage.removeItem(name);
 }
 
 export function setSession(name, content) {
-  if (!name && typeof name !== 'string' && content != null) return;
+  if (typeof name !== 'string' || !name || content == null) return;
   sessionStorage.setItem(name, JSON.stringify(content));
 }
 
 export function getSession(name) {
-  if (!name && typeof name !== 'string') return null;
+  if (typeof name !== 'string' || !name) return null;
   return JSON.parse(sessionStorage.getItem(name));
 }
 
 export function removeSession(name) {
-  if (!name && typeof name !== 'string') return;
+  if (typeof name !== 'string' || !name) return;
   sessionStorage.removeItem(name);
 }
